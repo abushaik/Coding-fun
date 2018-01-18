902734ee7577409e5b1639ef36c3f926fede659b
abushaik <abushaik5612@gmail.com>
2018-01-19 00:21:09 +0600

    string reverse

---
+++ string_reverse_manually.c
@@ -0,0 +1,22 @@
+#include<Stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+   char s[1000], r[1000];
+   int begin, end, count = 0;
+
+   printf("Input a string\n");
+   gets(s);
+   count=strlen(s); // Calculating string length
+   end = count - 1;
+   for (begin = 0; begin < count; begin++)
+   {
+      r[begin] = s[end]; //copy reverse string in a new string
+      end--;
+   }
+   r[begin] = '\0';
+   printf("%s\n", r);
+   return 0;
+
+}
