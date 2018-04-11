e504022ceae5ec2356b73c32e0f44c32a0ad6ada
Md. Abu Shaik <abushaik5612@gmail.com>
2018-04-11 15:09:15 +0600

    For Whitespace, Creating new line

---
+++ For_Whitespace_Creating _new_line.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str[80];
+	int i=0, len, j;
+	printf("Enter the String : ");
+	gets(str);
+	len=strlen(str);
+	for(i=0; i<len; i++)
+	{
+		if(str[i]==' ')
+		{
+           str[i]= '\n';
+		}
+	}
+	printf("String after removing spaces = %s\n", str);
+	getch();
+}
