3ccb9051e5987413d8614171c2e95d7441819f75
Md. Abu Shaik <abushaik5612@gmail.com>
2018-04-11 15:35:03 +0600

    Checking Comment.

---
+++ Checking Comment..c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str[80];
+	int i=0, len, j;
+	printf("Enter the String : ");
+	gets(str);
+	len=strlen(str);
+    if(str[0]=='/' && str[1]=='*' && str[len-1]=='/' && str[len-2]=='*' )
+    {
+        printf("Comment");
+    }
+    else if(str[0]=='/' && str[1]=='/')
+    {
+        printf("Comment");
+    }
+    else {
+        printf("No");
+    }
+	getch();
+}
