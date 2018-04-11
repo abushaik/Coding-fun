3c38685a9cc121406762d7da5fd9876ccd1156e6
Md. Abu Shaik <abushaik5612@gmail.com>
2018-04-11 16:48:40 +0600

    Simple Calculator with Checking Wrong input

---
+++ Simple Calculator with Checking Wrong input.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    char op;
+    char b;
+    int i,j;
+    printf("Enter 1st number?\n");
+    a= getchar();
+    if(a>='a' && a<='z' || a>='A' && a<='Z')
+    {
+        printf("Wrong input");
+        return 0;
+    }
+    else{
+      printf("Right input\n");
+    }
+
+
+    printf("Enter the 2nd number\n");
+    b=getchar();
+    if(b>='a' && b<='z' || b>='A' && b<='Z')
+    {
+        printf("Wrong input");
+        return 0;
+    }
+    else
+        printf("Right input and continue \n");
+
+    printf("Enter the operation you want?");
+    scanf("%c",&op);
+    if(op>='+' && op<='-' && op>='*' && op<='/')
+    {
+        printf("Right input");
+        return 0;
+    }
+    else
+        printf("Wrong input \n");
+
+
+}
